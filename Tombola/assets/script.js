1dc4ea782b8c4b4a4fba64878e94a746685d8344
Nicola Vettone <nicolavettone57@gmail.com>
2024-12-12 23:13:52 +0100

    aa

--- Tombola/assets/script.js
+++ Tombola/assets/script.js
@@ -1,8 +1,9 @@
 let numberArr = [];
+let utenteArr = [];
 
-function createNumbers() {
+function createNumbers(n) {
     let numeri = document.querySelector("#numberList");
-    for(let i=1; i<=76; i++) {
+    for(let i=1; i<=n; i++) {
         let cellNumber = document.createElement('div');
         cellNumber.classList.add('celle');
         cellNumber.innerText = i;
@@ -13,15 +14,46 @@ function createNumbers() {
 
 let randomNum = document.querySelector("button")
         randomNum.addEventListener('click', numRandom )
+
             function numRandom() {
-                for (let index = 1; index < 2; index++) {
-                    let num1 = Math.round(Math.random() * 76); 
-                    numberArr.push(num1)
-                    console.log(numberArr)
+                    let num1 = Math.round(Math.random() * 76)
+
+                    if (!numberArr.includes(num1) && num1 !== 0){ 
+                        numberArr.push(num1)
+
                     let allCells = document.querySelectorAll('.celle');
-                   allCells[num1 - 1].classList.add('estrai');
-                }
-                
-      }
-createNumbers();
+
+                    allCells[num1 - 1].classList.add('estrai');
+
+                    console.dir(numberArr)
+
+                    }else {
+                        numRandom()   
+                    }
+                    }             
+                    //randomNum.addEventListener('click', createCartella )
+                    function createCartella(n) {
+                        let numeriRandom = document.querySelector("#tabellaUtente");
+                        for(let i=1; i<=15; i++) {
+                            let num2 = Math.round(Math.random() * 76)
+                            let cellUser = document.createElement('div');
+                            cellUser.classList.add('celle');
+                            cellUser.innerText = num2;
+
+                            numeriRandom.appendChild(cellUser)
+
+                            let allCells = document.querySelectorAll('.celle');
+                            
+                            
+                        }}
+                            
+        
+                            //
+        
+                            //console.log(num2)
+        
+                            
+                            
+createNumbers(76);
+createCartella(76)
 
